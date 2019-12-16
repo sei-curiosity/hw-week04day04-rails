63f6b27b1c0ca1bb9a1ca213c82b172bc09cd8c5
alsubi <alsubaie22145@gmail.com>
2019-12-16 09:13:27 +0300

    hw

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -9,6 +9,7 @@ class ArticlesController < ApplicationController
     def edit
         @article = Article.find(params[:id])
     end
+    
 
     def new
         @article = Article.new
@@ -23,6 +24,15 @@ class ArticlesController < ApplicationController
           render 'new'
         end
     end
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+      end
 
     private
     def article_params
