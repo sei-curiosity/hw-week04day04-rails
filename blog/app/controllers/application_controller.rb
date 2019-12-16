1ac8bf60c8afac9def255efa83d02b292a0076e6
AlBattalSara <YOUR GITHUB EMAIL>
2019-12-16 06:51:52 +0300

    HW rails

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-    def new
-    end
+   
     
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles
+#   get 'welcome/index'
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   resources :articles
   
-  root 'welcome#index'
-end
+#   root 'welcome#index'
+# end
 
+
+
+# $ rails routes
+       Prefix Verb   URI Pattern                  Controller#Action
+welcome_index GET    /welcome/index(.:format)     welcome#index
+     articles GET    /articles(.:format)          articles#index
+              POST   /articles(.:format)          articles#create
+  new_article GET    /articles/new(.:format)      articles#new
+ edit_article GET    /articles/:id/edit(.:format) articles#edit
+      article GET    /articles/:id(.:format)      articles#show
+              PATCH  /articles/:id(.:format)      articles#update
+              PUT    /articles/:id(.:format)      articles#update
+              DELETE /articles/:id(.:format)      articles#destroy
+         root GET    /                            welcome#index
+end
