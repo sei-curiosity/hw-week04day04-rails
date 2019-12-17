848f0f056c56d2558b572ea59f69f76a7dd34410
Mohammed Al-Mohana <m.mohana2022@gmail.com>
2019-12-17 20:23:36 +0300

    finish

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,12 +1,10 @@
 class ArticlesController < ApplicationController
-    def index
-      @articles = Article.all
-    end
-   
-    def show
-      @article = Article.find(params[:id])
-    end
-   
+ 
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
     def new
       @article = Article.new
     end
