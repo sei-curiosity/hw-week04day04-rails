ec46fbcc548ee6e0a273125a593a7fb9ca54e726
Mohammed Al-Mohana <m.mohana2022@gmail.com>
2019-12-20 18:30:10 +0300

    user can create and delete comments of articles

--- blog/app/controllers/comments_conroller.rb
+++ blog/app/controllers/comments_conroller.rb
@@ -5,13 +5,16 @@ class CommentsController < ApplicationController
       @comment = @article.comments.create(comment_params)
       redirect_to article_path(@article)
     end
-
+    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
     def destroy
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
         redirect_to article_path(@article)
     end
-
+    private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
     
   end 
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,7 @@
 
 class Article < ApplicationRecord
-    validates :title, presence: true,
-                      length: { minimum: 5 }
+    has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                    length: { minimum: 5 } 
   end
+   
