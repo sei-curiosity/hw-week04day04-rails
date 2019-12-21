ac1cae9683f3346cceb5403d9742f06459b93efb
roush55 <ranaabumelha@gmail.com>
2019-12-21 11:53:05 +0300

    update hw

--- Mynewblog/config/routes.rb
+++ Mynewblog/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles
-  get 'articles/new',to: 'articles#new' 
-  get '/articles/:id(.:format)',to:'articles#show'
+  resources :articles do
+    resources :comments
+  end
+  # get 'articles/new',to: 'articles#new' 
+  # get '/articles/:id(.:format)',to:'articles#show'
+  # get '/articles(.:format)',to:          'articles#index'
+  # delete' /articles/:id(.:format)',to:      'articles#destroy'
 
   root 'welcome#index'
 end
